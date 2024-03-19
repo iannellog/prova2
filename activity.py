a965d3be74756be436804b7ea6e8e7ec082393f7
Giulio Iannello <g.iannello@unicampus.it>
2024-03-19 16:40:18 +0100

    aggiunto trattino

--- activity.py
+++ activity.py
@@ -6,7 +6,7 @@ class Activity:
     Represents an activity
     """
     def __init__(self, description: str, start_time: datetime, end_time: datetime):
-        self.description = description
+        self._description = description
 
 
     def start_time(self) -> datetime:
